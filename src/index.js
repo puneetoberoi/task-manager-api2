f9d1ed5d64fc00308612610e515adf5309b5c52b
puneetoberoi <59065458+puneetoberoi@users.noreply.github.com>
2020-04-20 13:25:22 +0530

    update1 - home page

--- src/index.js
+++ src/index.js
@@ -34,9 +34,9 @@ app.use('/tasks', tasks)
 app.use(express.static((__dirname, '../public')))
 
 
-// app.get('/', (req, res)=>{
-//     res.sendFile(path.join(__dirname, "../public/" + "index.html" ))
-// })
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, "../public/" + "index.html" ))
+})
 
 
 
